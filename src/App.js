6c17e7f831c8ccf577a25306a0b3e87c7e148808
Mohammad Arham Imam <imamarham10.com>
2023-02-12 22:08:53 +0530

    created socials & footer

--- src/App.js
+++ src/App.js
@@ -3,32 +3,41 @@ import cartIcon from './resources/shopping-cart.png';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage.js';
 import Productpage from './pages/Productpage.js';
+import Socials from './components/Socials.js';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <div>
-          <header className="header">
-            <div className="logo">
-              <span>
-                <Link to="/">
-                  <img src={logo} alt="logo" className="brand-icon" />
-                </Link>
-              </span>
-              <span className="brand-link">
-                <Link to="/">
-                  <span className="brand-name">KickPik</span>
-                </Link>
-              </span>
-            </div>
-            <Link to="/cart">
-              <div className="cart">
-                <div className="cart-icon">
-                  <img src={cartIcon} alt="cart" />
-                </div>
-                <div className="cart-text">Cart</div>
+          <header className="header-container">
+            <div className="socials">
+              <div className="footer-socials">
+                <Socials />
               </div>
-            </Link>
+            </div>
+            <div className="header">
+              <div className="logo">
+                <span>
+                  <Link to="/">
+                    <img src={logo} alt="logo" className="brand-icon" />
+                  </Link>
+                </span>
+                <span className="brand-link">
+                  <Link to="/">
+                    <span className="brand-name">KickPik</span>
+                  </Link>
+                </span>
+              </div>
+              <Link to="/cart">
+                <div className="cart">
+                  <div className="cart-icon">
+                    <img src={cartIcon} alt="cart" />
+                  </div>
+                  <div className="cart-text">Cart</div>
+                </div>
+              </Link>
+            </div>
           </header>
           <main>
             <Routes>
@@ -36,6 +45,9 @@ function App() {
               <Route path="/product/:name" element={<Productpage />} />
             </Routes>
           </main>
+          <footer className="footer">
+            <div className="footer-text">All Rights Reseved</div>
+          </footer>
         </div>
       </BrowserRouter>
     </>
