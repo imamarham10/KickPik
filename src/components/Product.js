0a017a333fa3492dbe0c19596cfe2d3b70bc5af2
Mohammad Arham Imam <imamarham10.com>
2023-02-12 13:17:33 +0530

    created product component

---
+++ src/components/Product.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Rating from './Rating.js';
+
+export default function Product(props) {
+  const { product } = props;
+  return (
+    <div key={product._id} className="product">
+      <div className="product-image">
+        <Link to={`/product/${product.name}`}>
+          <img src={product.image} alt={product.name} />
+        </Link>
+      </div>
+      <div className="product-info">
+        <div className="product-name-price">
+          <Link to={`/product/${product.name}`}>
+            <span className="product-name">
+              <strong>{product.name}</strong>
+            </span>
+          </Link>
+          <span>
+            <strong>${product.price}</strong>
+          </span>
+        </div>
+        <div className="product-gender">
+          <span>{product.gender}</span>
+        </div>
+        <div>
+          <Rating
+            rating={product.rating}
+            numReviews={product.numberOfReviews}
+          />
+        </div>
+        <div>
+          <button className="product-addToCart-button">
+            <span>Add to Cart</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Product from '../components/Product.js';
 import Rating from '../components/Rating.js';
 import data from '../Data.js';
 export default function Homepage() {
@@ -19,39 +20,7 @@ export default function Homepage() {
         <h1>Featured Product</h1>
         <div className="products">
           {data.products.map((product) => (
-            <div key={product._id} className="product">
-              <div className="product-image">
-                <Link to={`/product/${product.name}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-              </div>
-              <div className="product-info">
-                <div className="product-name-price">
-                  <Link to={`/product/${product.name}`}>
-                    <span className="product-name">
-                      <strong>{product.name}</strong>
-                    </span>
-                  </Link>
-                  <span>
-                    <strong>${product.price}</strong>
-                  </span>
-                </div>
-                <div className="product-gender">
-                  <span>{product.gender}</span>
-                </div>
-                <div>
-                  <Rating
-                    rating={product.rating}
-                    numReviews={product.numberOfReviews}
-                  />
-                </div>
-                <div>
-                  <button className="product-addToCart-button">
-                    <span>Add to Cart</span>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <Product key={product._id} product={product} />
           ))}
         </div>
       </div>
