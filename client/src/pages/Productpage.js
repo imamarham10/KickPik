fb1d29a593b1f8d92b45c743f44949fc6c864bdc
Mohammad Arham Imam <imamarham10.com>
2023-04-26 16:43:30 +0530

    ProductPage fetch request update

--- client/src/pages/Productpage.js
+++ client/src/pages/Productpage.js
@@ -36,7 +36,7 @@ export default function Productpage() {
   const fetchProduct = () => {
     dispatch({ type: 'FETCH_REQUEST' });
     try {
-      ky.get(`/api/products/${id}`)
+      ky.get(`https://kickpik-backend.vercel.app/api/products/${id}`)
         .then((res) => res.json())
         .then((res) => dispatch({ type: 'FETCH_SUCCESS', payload: res }));
     } catch (err) {
