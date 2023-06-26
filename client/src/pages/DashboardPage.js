bd03ba531f24c25e8238ec8f3b492c69593fd96e
Mohammad Arham Imam <imamarham10@gmail.com>
2023-06-26 17:48:58 +0530

    fixed deployemnet issue

---
+++ client/src/pages/DashboardPage.js
@@ -0,0 +1,85 @@
+import React, { useContext, useEffect, useReducer } from "react";
+import { Store } from "../Store.js";
+import { getError } from "../util.js";
+import axios from "axios";
+import Loading from "../components/Loading";
+import ErrorMessage from "../components/Message";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, summary: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+export default function DashboardPage() {
+  const [{ loading, summary, error }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(userInfo.token);
+        const { data } = await axios.get('/api/orders/summary', {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err),
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+  return (
+    <div>
+      <h1 className="font-bold font-nunito text-4xl">Dashboard</h1>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <ErrorMessage variant="danger">{error}</ErrorMessage>
+      ) : (
+        <>
+          <div className="flex">
+            <div className="flex-col">
+              <div>
+                {summary.users && summary.users[0]
+                  ? summary.users[0].numUsers
+                  : 0}
+              </div>
+              <div>Users</div>
+            </div>
+            <div className="flex-col">
+              <div>
+                {summary.orders && summary.users[0]
+                  ? summary.orders[0].numOrders
+                  : 0}
+              </div>
+              <div>Orders</div>
+            </div>
+            <div className="flex-col">
+              <div>
+                $
+                {summary.orders && summary.users[0]
+                  ? summary.orders[0].totalSales.toFixed(2)
+                  : 0}
+              </div>
+              <div>Orders</div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -3,18 +3,21 @@
 // import Order from '../models/orderModel.js';
 // import { isAuth } from '../util.js';
 
-const express = require('express');
-const expressAsyncHandler = require('express-async-handler');
-const Order = require('../models/orderModel');
-const { isAuth } = require('../util');
+const express = require("express");
+const expressAsyncHandler = require("express-async-handler");
+const Order = require("../models/orderModel");
+const User = require("../models/userModel");
+const { isAuth, isAdmin } = require("../util");
+const Product = require("../models/productModel");
+const mongoose = require("mongoose");
 
 const orderRouter = express.Router();
 orderRouter.post(
-  '/',
+  "/",
   isAuth,
   expressAsyncHandler(async (req, res) => {
     if (req.body.orderItems.length === 0) {
-      res.status(400).send({ message: 'Cart is empty' });
+      res.status(400).send({ message: "Cart is empty" });
     } else {
       const newOrder = new Order({
         orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
@@ -28,13 +31,13 @@ orderRouter.post(
       });
 
       const order = await newOrder.save();
-      res.status(201).send({ message: 'New Order Created', order });
+      res.status(201).send({ message: "New Order Created", order });
     }
   })
 );
 
 orderRouter.get(
-  '/mine',
+  "/mine",
   isAuth,
   expressAsyncHandler(async (req, res) => {
     const orders = await Order.find({ user: req.user._id });
@@ -43,7 +46,7 @@ orderRouter.get(
 );
 
 orderRouter.put(
-  '/:id/pay',
+  "/:id/pay",
   isAuth,
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
@@ -58,22 +61,71 @@ orderRouter.put(
       };
 
       const updatedOrder = await order.save();
-      res.send({ message: 'Order Paid', order: updatedOrder });
+      res.send({ message: "Order Paid", order: updatedOrder });
     } else {
-      res.status(404).send({ message: 'Order Not Found' });
+      res.status(404).send({ message: "Order Not Found" });
     }
   })
 );
 
 orderRouter.get(
-  '/:id',
+  "/:id",
   isAuth,
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
     if (order) {
       res.send(order);
     } else {
-      res.status(404).send({ message: 'Order Not Found' });
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRouter.get(
+  "/summary",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const orders = await Order.aggregate([
+        {
+          $group: {
+            _id: null,
+            numOrders: { $sum: 1 },
+            totalSales: { $sum: "$totalPrice" },
+          },
+        },
+      ]);
+      const users = await User.aggregate([
+        {
+          $group: {
+            _id: null,
+            numUsers: { $sum: 1 },
+          },
+        },
+      ]);
+      const dailyOrders = await Order.aggregate([
+        {
+          $group: {
+            _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+            orders: { $sum: 1 },
+            sales: { $sum: "$totalPrice" },
+          },
+        },
+        { $sort: { _id: 1 } },
+      ]);
+      const productCategories = await Product.aggregate([
+        {
+          $group: {
+            _id: "$category",
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+      res.send({ users, orders, dailyOrders, productCategories });
+    } catch (error) {
+      console.error("Error in summary endpoint:", error);
+      res.status(500).send({ message: "Internal Server Error" });
     }
   })
 );
